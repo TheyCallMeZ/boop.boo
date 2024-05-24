9e10d07734af526ac48e607c51265e0a6f01b871
Stephen Zura <swzura@gmail.com>
2024-05-24 07:01:15 -0400

    Update config and package json to run a after build script

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/boop.boo/" : "/",
-  chainWebpack: (config) => {
-    config.plugin("copy").use(CopyPlugin, [
-      {
-        patterns: [{ from: "public/index.html", to: "404.html" }],
-      },
-    ]);
-  },
 });
