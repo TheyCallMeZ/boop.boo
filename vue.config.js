98379e4af0bfe8d12b5537a4b65f7948cf7d1908
Stephen Zura <swzura@gmail.com>
2024-05-24 06:38:38 -0400

    Update vue config

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/boop.boo/" : "/",
   chainWebpack: config => {
     config.plugin('copy')
       .use(CopyPlugin, [
