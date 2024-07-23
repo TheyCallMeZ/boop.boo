14815ff0197404c6c85c20dd9f0a9cd138630997
Stephen Zura <swzura@gmail.com>
2024-07-23 00:12:23 -0400

    Initial Dawntrail Extreme List, setup dropdown to change expansions

--- src/components/ffxiv_fights.vue
+++ src/components/ffxiv_fights.vue
@@ -1,9 +1,32 @@
-﻿<script setup>
-import trials from "@/assets/data/endwalker-extreme-fights.json";
-console.log(trials);
+﻿<script>
+import stormblood from "@/assets/data/stormblood-extreme-fights.json";
+import endwalker from "@/assets/data/endwalker-extreme-fights.json";
+import dawntrail from "@/assets/data/dawntrail-extreme-fights.json";
+export default {
+  data() {
+    return {
+      selectedTrial: "dawntrail",
+      dataFiles: {
+        stormblood: stormblood,
+        endwalker: endwalker,
+        dawntrail: dawntrail,
+      },
+    };
+  },
+  computed: {
+    selectedTrialData: function () {
+      return this.dataFiles[this.selectedTrial];
+    },
+  },
+};
 </script>
 
 <template>
+  <select v-model="selectedTrial">
+    <option value="stormblood">Stormblood</option>
+    <option value="endwalker">Endwalker</option>
+    <option value="dawntrail">Dawntrail</option>
+  </select>
   <table>
     <thead>
       <tr>
@@ -13,7 +36,7 @@ console.log(trials);
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(trial, index) in trials.extreme" :key="index">
+      <tr v-for="(trial, index) in selectedTrialData.extreme" :key="index">
         <td>
           {{ trial.fightName }}<br />
           <img :src="trial.imgPath" alt="{{ trial.fightName }}" />
